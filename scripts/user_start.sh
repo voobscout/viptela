4c289f3795669fb2fd2371784f594239d7626738
voobscout <voobscout@archlinux.info>
2017-07-18 17:35:07 +0200

    ..

--- scripts/user_start.sh
+++ scripts/user_start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+DISKS=("b" "c" "d" "e" "f")
+RAID=$(mdadm --detail --scan | awk '{print $2}')
+
 _install_essentials() {
     local apps=("nfs-common" "mdadm" "parted")
     for i in ${apps[@]}; do
@@ -7,23 +10,21 @@ _install_essentials() {
 }
 
 _mk_raid() {
-    local disks=("b" "c" "d" "e" "f")
     local md=(${md[@]})
-    for i in "${disks[@]}"; do
-        mdadm --misc --zero-superblock /dev/sd$i
-        echo "Done with /dev/sd$i"
+    for i in "${DISKS[@]}"; do
         md=(${md[@]} /dev/sd$i)
     done
-    mdadm --create --verbose --level=0 --metadata=1.2 --chunk=512 --raid-devices=${#md[@]} /dev/md0 ${md[@]}
+    mdadm --create --verbose --level=0 --metadata=1.2 --chunk=1024 --raid-devices=${#md[@]} /dev/md0 ${md[@]}
 }
 
-_mount_nfs() {
-    mkdir -p /opt/config_store
-    mkdir -p /var/etcd/backups
-
-    rpcbind
-    # mount magrathea:/exports/config_store /opt/config_store
-    # mount --rbind /opt/config_store/rancher/k8etcd.bkp /var/etcd/backups
+_rm_raid() {
+    if [[ -n $RAID ]]; then
+        mdadm --stop $RAID
+        for i in "${DISKS[@]}"; do
+            mdadm --misc --zero-superblock /dev/sd$i
+        done
+    fi
 }
 
 _install_essentials
+[[ ! -n $RAID ]] && _mk_raid
