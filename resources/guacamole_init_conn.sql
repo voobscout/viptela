a4c679f7c935b57c87dd5139c9c3339da3311172
voobscout <voobscout@archlinux.info>
2017-07-26 13:07:28 +0200

    ..

--- resources/guacamole_init_conn.sql
+++ resources/guacamole_init_conn.sql
@@ -1,9 +1,19 @@
 -- Add User
+-- Generate salt
+SET @salt = UNHEX(SHA2(UUID(), 256));
+
+-- Create user and hash password with salt
+INSERT INTO guacamole_user (username, password_salt, password_hash)
+VALUES ('sdwan-demo', @salt, UNHEX(SHA2(CONCAT('sdwan-demo', HEX(@salt)), 256)));
 
 -- Create connection
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('test', 'vnc');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Desktop', 'vnc');
 SET @id = LAST_INSERT_ID();
 
 -- Add parameters
-INSERT INTO guacamole_connection_parameter VALUES (@id, 'hostname', 'localhost');
-INSERT INTO guacamole_connection_parameter VALUES (@id, 'port', '5901');
+INSERT INTO guacamole_connection_parameter VALUES (@id, 'hostname', 'desktop');
+INSERT INTO guacamole_connection_parameter VALUES (@id, 'port', '5900');
+
+
+--
+INSERT INTO `guacamole_connection` VALUES (1,'desktop',NULL,'vnc',NULL,NULL);
